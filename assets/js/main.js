7bc960958d80a45c3c009f2a63d690bbef90856e
Het Prajapati <hprajapati@albany.edu>
2021-07-23 23:19:19 +0530

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,7 +262,6 @@ gsap.from(".home__scroll", {
 });
 
 // Scroll Animations
-
 gsap.from("#about__title", {
   scrollTrigger: "#cvButton",
   opacity: 0,
@@ -372,7 +371,7 @@ gsap.from("#qualification__title", {
   scrollTrigger: "#trigger1",
   opacity: 0,
   duration: 1,
-  delay: 0.5,
+  delay: 0.3,
   y: 10,
 });
 
@@ -380,7 +379,7 @@ gsap.from("#qualification__subtitle", {
   scrollTrigger: "#trigger1",
   opacity: 0,
   duration: 1,
-  delay: 0.7,
+  delay: 0.5,
   y: 10,
 });
 
@@ -388,7 +387,7 @@ gsap.from("#pesto", {
   scrollTrigger: "#trigger1",
   opacity: 0,
   duration: 1,
-  delay: 1.5,
+  delay: 1.0,
   y: 10,
 });
 
@@ -396,33 +395,19 @@ gsap.from("#salsa", {
   scrollTrigger: "#trigger1",
   opacity: 0,
   duration: 1,
-  delay: 1.9,
+  delay: 1.2,
   y: 10,
 });
-
-// gsap.from(
-//   ".qualification__line",
-//   1,
-//   {
-//     scrollTrigger: "#trigger1",
-//     transformOrigin: "bottom",
-//     scaleY: 4,
-//     opacity: 0,
-//     duration: 1,
-//     delay: 2.1,
-//   },
-//   0
-// );
 
 gsap.from("#education", {
   scrollTrigger: "#trigger1",
   opacity: 0,
   duration: 1,
-  delay: 3,
+  delay: 1.5,
   y: 10,
 });
 
-//Services Animations
+// Services Animations
 
 gsap.from("#service__title", {
   scrollTrigger: "#portfolio",
@@ -440,10 +425,26 @@ gsap.from("#service__subtitle", {
   y: 10,
 });
 
-gsap.from(".services__content", {
+// gsap.from("#services", {
+//   scrollTrigger: "#portfolio",
+//   opacity: 0,
+//   duration: 1,
+//   delay: 0.8,
+//   x: -30,
+// });
+
+gsap.from("#portfolio", {
   scrollTrigger: "#portfolio",
   opacity: 0,
   duration: 1,
-  delay: 0.8,
-  y: 30,
+  delay: 0.3,
+  y: 20,
+});
+
+gsap.from("#pasture", {
+  scrollTrigger: "#pim",
+  opacity: 0,
+  duration: 1,
+  delay: 0.3,
+  y: 20,
 });
